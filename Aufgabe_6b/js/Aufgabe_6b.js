5c4d94af165d5ee896e902facf3b956d547285a2
jakobviets <jakobviets@jakobs-air-5.hfu-open.fu.hs-furtwangen.de>
2017-05-03 11:18:11 +0200

    Aufgabe 6 push

---
+++ Aufgabe_6b/js/Aufgabe_6b.js
@@ -0,0 +1,39 @@
+/*
+Aufgabe: 6b
+Name: Jakob Viets
+Matrikel: 254780
+Datum: 02.05.17
+    
+Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
+*/
+var StudiVZ;
+(function (StudiVZ) {
+    var students = [];
+    var stop = false;
+    while (!stop) {
+        var action = prompt("Datensatz anlegen (n), abfragen(a) oder Programm beenden (s)\nn,a oder s eingeben");
+        switch (action) {
+            case "n":
+            case "N":
+                var input = prompt("Eingabe (jeweils mit Komma getrennt) von\nMatrikelnummer, Name, Vorname, Alter, Geschlecht (0 oder 1) und Kommentar");
+                alert(saveData(input));
+                break;
+            case "a":
+            case "A":
+                var matrikel = parseInt(prompt("Eingabe Matrikelnummer"));
+                alert(queryData(matrikel));
+                break;
+            case "s":
+            case "S":
+                stop = true;
+        }
+    }
+    function saveData(_input) {
+        let InputArray = input.split(",");
+        return "Hier fehlt noch der richtige Code...";
+    }
+    function queryData(_matrikel) {
+        return "Hier fehlt noch der richtige Code...";
+    }
+})(StudiVZ || (StudiVZ = {}));
+//# sourceMappingURL=Aufgabe_6b.js.map
---
+++ Aufgabe_6b/js/Aufgabe_6b.ts
@@ -0,0 +1,51 @@
+/* 
+Aufgabe: 6b
+Name: Jakob Viets
+Matrikel: 254780
+Datum: 02.05.17
+    
+Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
+*/
+
+namespace StudiVZ {
+    interface StudentData {
+        // hier ist noch die richtige Datenstruktur festzulegen
+        Matrikelnummer: number;
+        Name: string;
+        Vorname: string;
+        Alter: number;
+        Geschlecht: boolean;
+        Kommentar: string;
+    }
+    var students: StudentData[] = [];
+    var stop: boolean = false;
+
+    while (!stop) {
+        var action: string = prompt("Datensatz anlegen (n), abfragen(a) oder Programm beenden (s)\nn,a oder s eingeben");
+
+        switch (action) {
+            case "n":
+            case "N":
+                var input: string = prompt("Eingabe (jeweils mit Komma getrennt) von\nMatrikelnummer, Name, Vorname, Alter, Geschlecht (0 oder 1) und Kommentar");
+                alert(saveData(input));
+                break;
+            case "a":
+            case "A":
+                var matrikel: number = parseInt(prompt("Eingabe Matrikelnummer"));
+                alert(queryData(matrikel));
+                break;
+            case "s":
+            case "S":
+                stop = true;
+        }
+    }
+
+    function saveData(_input: string): string {
+        let InputArray: string[] = input.split(",");
+        
+        return "Hier fehlt noch der richtige Code...";
+    }
+    function queryData(_matrikel: number): string {
+        return "Hier fehlt noch der richtige Code...";
+    }
+}
