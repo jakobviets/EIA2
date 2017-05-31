36ee1a3e8037f53ad925e1089c69fb7a703ca531
jakobviets <jakobviets@jakobs-air-5.hfu-open.fu.hs-furtwangen.de>
2017-06-01 00:03:11 +0200

    Warenkorb und Header weiterentwickelt

--- Aufgabe_9/js/Aufgabe_9.js
+++ Aufgabe_9/js/Aufgabe_9.js
@@ -2,28 +2,36 @@ var saveInput;
 (function (saveInput) {
     window.addEventListener("load", init);
     let sorts = ["Erdbeer", "Himbeer", "Melone", "Mango", "Pfirsich", "Schokolade", "Haselnuss", "Walnuss", "Nougat"];
-    let Produktauswahl;
+    let Eissorten;
+    let Toppings;
+    let Darbietungsform;
     let toppings = ["Sahne", "Streusel", "Schokosoße", "Erdbeersoße"];
     let inputsEis = [];
     let inputsTopping = [];
     let darbietung = ["Waffel", "Becher"];
     let inputsDarbietung = [];
     let Warenkorb;
+    let Kundendaten = document.getElementById("checkData");
     function init() {
-        Produktauswahl = document.getElementById("Auswahl");
+        Eissorten = document.getElementById("Eissorten");
+        Toppings = document.getElementById("Toppings");
+        Darbietungsform = document.getElementById("Darbietungsform");
         Warenkorb = document.getElementById("Warenkorb");
         createInputs();
         createCheckboxes();
         createRadios();
-        Produktauswahl.addEventListener("change", change);
-        console.log(inputsEis);
-        console.log(inputsTopping);
-        console.log(inputsDarbietung);
+        Eissorten.addEventListener("change", change);
+        Toppings.addEventListener("change", change);
+        Darbietungsform.addEventListener("change", change);
+        Kundendaten.addEventListener("click", checkData);
+    }
+    function checkData(_event) {
+        let target = _event.target;
+        console.log("Changed " + target.name + " to " + target.value);
     }
     function createInputs() {
         // Erstelle pro Sorte Kuchen einen Input
         for (let i = 0; i < sorts.length; i++) {
-            console.log(sorts[i]);
             createInput(sorts[i]);
         }
     }
@@ -37,13 +45,13 @@ var saveInput;
         input.type = "number";
         input.min = "0";
         input.value = "0";
-        Produktauswahl.appendChild(label);
+        label.id = _sort;
+        Eissorten.appendChild(label);
         inputsEis.push(input);
     }
     function createCheckboxes() {
         // Erstelle pro Sorte Kuchen einen Input
         for (let i = 0; i < toppings.length; i++) {
-            console.log(toppings[i]);
             createCheckbox(toppings[i]);
         }
     }
@@ -55,13 +63,13 @@ var saveInput;
         label.appendChild(input);
         // Die Art des Inputs wird über den Typ definiert
         input.type = "checkbox";
-        Produktauswahl.appendChild(label);
+        label.id = _topping;
+        Toppings.appendChild(label);
         inputsTopping.push(input);
     }
     function createRadios() {
         // Erstelle pro Sorte Kuchen einen Input
         for (let i = 0; i < darbietung.length; i++) {
-            console.log(darbietung[i]);
             createRadio(darbietung[i]);
         }
     }
@@ -75,7 +83,8 @@ var saveInput;
         input.type = "radio";
         input.name = "darbietung";
         input.required = true;
-        Produktauswahl.appendChild(label);
+        label.id = _darbietung;
+        Darbietungsform.appendChild(label);
         inputsDarbietung.push(input);
     }
     // Wenn sich etwas ändert, summiere die Werte aller inputs auf und gebe
@@ -95,23 +104,28 @@ var saveInput;
                 sum += 1.5;
             }
         }
-        console.log(sum);
         changeWarenkorb(sum);
     }
     function changeWarenkorb(_sum) {
+        let Warenliste = document.getElementById("Warenliste");
+        Warenliste.innerText = "";
         for (let i = 0; i < inputsEis.length; i++) {
-            if (parseInt(inputsEis[i].value) > 0) { }
+            if (parseInt(inputsEis[i].value) > 0) {
+                Warenliste.innerText += sorts[i];
+            }
         }
         for (let i = 0; i < inputsTopping.length; i++) {
-            if (inputsTopping[i].checked) { }
+            if (inputsTopping[i].checked) {
+                Warenliste.innerText += toppings[i];
+            }
         }
         for (let i = 0; i < inputsDarbietung.length; i++) {
-            if (inputsDarbietung[i].checked) { }
+            if (inputsDarbietung[i].checked) {
+                Warenliste.innerText += darbietung[i];
+            }
         }
         let summeHtml = document.getElementById("Summe");
-        let Ausgabe = document.createElement("p");
-        Ausgabe.innerText = _sum.toString();
-        summeHtml.appendChild(Ausgabe);
+        summeHtml.innerText = _sum.toString() + " €";
     }
 })(saveInput || (saveInput = {}));
 //# sourceMappingURL=Aufgabe_9.js.map
--- Aufgabe_9/js/Aufgabe_9.ts
+++ Aufgabe_9/js/Aufgabe_9.ts
@@ -2,33 +2,45 @@ namespace saveInput {
     window.addEventListener("load", init);
 
     let sorts: string[] = ["Erdbeer", "Himbeer", "Melone", "Mango", "Pfirsich", "Schokolade", "Haselnuss", "Walnuss", "Nougat"];
-    let Produktauswahl: HTMLElement;
+    let Eissorten: HTMLElement;
+    let Toppings: HTMLElement;
+    let Darbietungsform: HTMLElement;
     let toppings: string[] = ["Sahne", "Streusel", "Schokosoße", "Erdbeersoße"];
     let inputsEis: HTMLInputElement[] = [];
     let inputsTopping: HTMLInputElement[] = [];
     let darbietung: string[] = ["Waffel", "Becher"];
     let inputsDarbietung: HTMLInputElement[] = [];
     let Warenkorb: HTMLElement;
+    let Kundendaten: HTMLElement = document.getElementById("checkData");
 
 
     function init(): void {
-        Produktauswahl = document.getElementById("Auswahl");
+        Eissorten = document.getElementById("Eissorten");
+        Toppings = document.getElementById("Toppings");
+        Darbietungsform = document.getElementById("Darbietungsform");
         Warenkorb = document.getElementById("Warenkorb");
 
         createInputs();
         createCheckboxes();
         createRadios();
 
-        Produktauswahl.addEventListener("change", change);
-        console.log(inputsEis);
-        console.log(inputsTopping);
-        console.log(inputsDarbietung);
+        Eissorten.addEventListener("change", change);
+        Toppings.addEventListener("change", change);
+        Darbietungsform.addEventListener("change", change);
+        
+        Kundendaten.addEventListener("click", checkData);
     }
 
+    
+    function checkData(_event: Event): void {
+        let target: HTMLInputElement = <HTMLInputElement>_event.target;
+        console.log("Changed " + target.name + " to " + target.value);
+    }
+    
+    
     function createInputs(): void {
         // Erstelle pro Sorte Kuchen einen Input
         for (let i: number = 0; i < sorts.length; i++) {
-            console.log(sorts[i]);
             createInput(sorts[i]);
         }
     }
@@ -44,8 +56,9 @@ namespace saveInput {
         input.type = "number";
         input.min = "0";
         input.value = "0";
+        label.id = _sort;
 
-        Produktauswahl.appendChild(label);
+        Eissorten.appendChild(label);
         inputsEis.push(input);
     }
 
@@ -53,7 +66,6 @@ namespace saveInput {
     function createCheckboxes(): void {
         // Erstelle pro Sorte Kuchen einen Input
         for (let i: number = 0; i < toppings.length; i++) {
-            console.log(toppings[i]);
             createCheckbox(toppings[i]);
         }
     }
@@ -67,15 +79,15 @@ namespace saveInput {
         label.appendChild(input);
         // Die Art des Inputs wird über den Typ definiert
         input.type = "checkbox";
+        label.id= _topping;
 
-        Produktauswahl.appendChild(label);
+        Toppings.appendChild(label);
         inputsTopping.push(input);
     }
     
     function createRadios(): void {
         // Erstelle pro Sorte Kuchen einen Input
         for (let i: number = 0; i < darbietung.length; i++) {
-            console.log(darbietung[i]);
             createRadio(darbietung[i]);
         }
     }
@@ -91,8 +103,9 @@ namespace saveInput {
         input.type = "radio";
         input.name = "darbietung";
         input.required = true;
+        label.id = _darbietung;
 
-        Produktauswahl.appendChild(label);
+        Darbietungsform.appendChild(label);
         inputsDarbietung.push(input);
     }    
     
@@ -115,7 +128,6 @@ namespace saveInput {
             if(inputsDarbietung[i].checked)
             {sum += 1.5;}
         }        
-        console.log(sum);
         
         changeWarenkorb(sum);
 
@@ -123,38 +135,34 @@ namespace saveInput {
     
     function changeWarenkorb(_sum: number): void {
         
+        let Warenliste: HTMLElement = document.getElementById("Warenliste");
+        Warenliste.innerText = "";
+        
         for (let i: number = 0; i < inputsEis.length; i++) {
             if(parseInt(inputsEis[i].value) > 0)
-            {}
+            {
+                Warenliste.innerText += sorts[i];
+            }
         }
         for (let i: number = 0; i < inputsTopping.length; i++) {
             if(inputsTopping[i].checked)
-            {}
+            {
+                Warenliste.innerText += toppings[i];
+            }
         }
         for (let i: number = 0; i < inputsDarbietung.length; i++) {
             if(inputsDarbietung[i].checked)
-            {}
+            {
+                Warenliste.innerText += darbietung[i];
+            }
         }    
         
         
         let summeHtml: HTMLElement = document.getElementById("Summe");
-        let Ausgabe: HTMLElement = document.createElement("p");
-        Ausgabe.innerText = _sum.toString();
-        summeHtml.appendChild(Ausgabe);
+        summeHtml.innerText = _sum.toString() + " €";
         
         
     }
     
     
 }
-
-
-
-
-
-
-
-
-
-
-
